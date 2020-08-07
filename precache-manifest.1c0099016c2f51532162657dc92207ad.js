aa76acca3534e4876f0b0f952c058585f31ce2b1
mattbarron0917 <matthew.barron@macaulay.cuny.edu>
2020-08-07 19:21:43 -0400

    Updates

---
+++ precache-manifest.1c0099016c2f51532162657dc92207ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8954779ae61989b49e29917f8fc88d",
+    "url": "/Matt-Page/index.html"
+  },
+  {
+    "revision": "03de06c01d05538ac402",
+    "url": "/Matt-Page/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "73e5e3750cc8a43f4b31",
+    "url": "/Matt-Page/static/css/main.15208d3e.chunk.css"
+  },
+  {
+    "revision": "03de06c01d05538ac402",
+    "url": "/Matt-Page/static/js/2.1acbfe72.chunk.js"
+  },
+  {
+    "revision": "cf7302ab57415842ba2bf20f719dee93",
+    "url": "/Matt-Page/static/js/2.1acbfe72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e5e3750cc8a43f4b31",
+    "url": "/Matt-Page/static/js/main.d9440efe.chunk.js"
+  },
+  {
+    "revision": "e9b86a62d9c2a183471b",
+    "url": "/Matt-Page/static/js/runtime-main.5bca8df1.js"
+  },
+  {
+    "revision": "d3a65c99f7fad0533710a1e275d1dbd1",
+    "url": "/Matt-Page/static/media/me.d3a65c99.png"
+  }
+]);
